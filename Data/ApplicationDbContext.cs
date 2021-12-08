74045b8c15b12df4ccae1d95e967093cd7054ed7
Andre Baltieri <andrebaltieri@hotmail.com>
2021-12-08 11:58:54 -0300

    Finalizado a demo

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlzAuth.Data;
+
+public class ApplicationDbContext : IdentityDbContext<
+    IdentityUser<Guid>,
+    IdentityRole<Guid>,
+    Guid, IdentityUserClaim<Guid>,
+    IdentityUserRole<Guid>,
+    IdentityUserLogin<Guid>,
+    IdentityRoleClaim<Guid>,
+    IdentityUserToken<Guid>>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
